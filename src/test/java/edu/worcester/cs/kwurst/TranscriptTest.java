9a86e36ae2a68710f91900346e886dd5a9e747be
jshatos <jshatos@worcester.edu>
2015-10-17 22:24:16 -0400

    Changed_Tests
    Changed tests to those we made in unit testing 2

--- src/test/java/edu/worcester/cs/kwurst/TranscriptTest.java
+++ src/test/java/edu/worcester/cs/kwurst/TranscriptTest.java
@@ -5,13 +5,30 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.worcester.cs.kwurst.Transcript.Semester;
+
 public class TranscriptTest {
 
-	Transcript t1;
+	private Transcript t1;
+	private Student student1;
+	private Student student2;
+	
+	private Course course1, course2;
+	private Grade gradeA, gradeCplus, gradeE, gradeIP;
 	
 	@Before
 	public void setUp() throws Exception {
 		t1 = new Transcript();
+		student1 = new Student("Jane", "Smith");
+		student2 = new Student("Jonathan", "Shatos");
+		
+		course1 = new Course("CS", 201, "Data Mining", 3);
+		course2 = new Course("Math", 301, "Calculus", 4);
+		
+		gradeA = new Grade("A");
+		gradeCplus = new Grade("C+");
+		gradeE = new Grade("E");
+		gradeIP = new Grade("IP");
 	}
 
 	@Test
@@ -30,5 +47,75 @@ public class TranscriptTest {
 				Transcript.Semester.FALL, 2015, new Grade("IP"));
 		assertEquals(3.0, t1.getGpa(), 0.0);	
 	}  
-
+	
+	@Test
+	public void testMajorComplete(){
+		student1.setMajorComplete(true);
+		assertTrue(student1.getMajorComplete());
+	}
+	
+	@Test
+	public void testGetLetterGrade(){
+		assertEquals("A", gradeA.getLetterGrade());
+		assertEquals("C+", gradeCplus.getLetterGrade());
+		assertEquals("E", gradeE.getLetterGrade());
+		assertEquals("IP", gradeIP.getLetterGrade());
+	}
+	
+	@Test
+	public void testGetNumericGrade(){
+		assertEquals(4.0, gradeA.getNumericGrade(),0);
+		assertEquals(2.3, gradeCplus.getNumericGrade(),0);
+		assertEquals(0, gradeE.getNumericGrade(),0);
+		assertEquals(0, gradeIP.getNumericGrade(),0);
+	}
+	
+	@Test
+	public void testGetDepartment(){
+		assertEquals("CS", course1.getDepartment());
+		assertEquals("Math", course2.getDepartment());
+		course2.setDepartment("Biology");
+		assertEquals("Biology", course2.getDepartment());
+		
+	}
+	
+	@Test
+	public void testGetNumber(){
+		assertEquals(201, course1.getNumber());
+		assertEquals(301, course2.getNumber());
+		course2.setNumber(999);
+		assertEquals(999, course2.getNumber());
+		
+	}
+	
+	@Test
+	public void testGetTitle(){
+		assertEquals("Data Mining", course1.getTitle());
+		assertEquals("Calculus", course2.getTitle());
+		course2.setTitle("Cell Growth");
+		assertEquals("Cell Growth", course2.getTitle());
+		
+	}
+	
+	@Test
+	public void testGetCredits(){
+		assertEquals(3, course1.getCredits());
+		assertEquals(4, course2.getCredits());
+		course2.setCredits(999);
+		assertEquals(999, course2.getCredits());
+		
+	}
+	
+	@Test
+	public void testAddCourse(){
+		Course c = new Course("CS", 443, "Quality Assurance", 3);
+		student1.addCourse(c, Semester.FALL, 2015, gradeA);
+		assertTrue(student1.getCurrentEarnedCr() == 3);
+	}
+	
+	@Test
+	public void testGetTranscript(){
+		student2.addCourse(course2,Semester.SPRING, 2016, gradeA);
+		assertTrue(student2.getTranscript() != null);
+	}
 }
